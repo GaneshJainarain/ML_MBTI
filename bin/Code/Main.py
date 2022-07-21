4d29cc86d5d4923392f9fc67b816caeab172d578
Ganesh Jainarain <RicheyJay2@gmail.com>
2022-07-21 16:56:14 -0400

    added to the code

---
+++ bin/Code/Main.py
@@ -0,0 +1,59 @@
+#MBTI Personality Prediction Machine Learning Model¶
+# Data Analysis
+import pandas as pd
+import numpy as np
+from numpy import asarray
+from numpy import savetxt
+from numpy import loadtxt
+import pickle as pkl
+from scipy import sparse
+
+# Data Visualization
+import seaborn as sns
+import matplotlib.pyplot as plt
+import wordcloud
+from wordcloud import WordCloud, STOPWORDS
+
+# Text Processing
+import re
+import itertools
+import string
+import collections
+from collections import Counter
+from sklearn.preprocessing import LabelEncoder
+import nltk
+from nltk.classify import NaiveBayesClassifier
+from nltk.corpus import stopwords
+from nltk import word_tokenize
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer, WordNetLemmatizer
+
+# Machine Learning packages
+import sklearn
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+import sklearn.cluster as cluster
+from sklearn.manifold import TSNE
+
+# Model training and evaluation
+from sklearn.model_selection import train_test_split
+
+#Models
+from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import SGDClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from xgboost import XGBClassifier
+from xgboost import plot_importance
+
+#Metrics
+from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error, accuracy_score, balanced_accuracy_score
+from sklearn.metrics import precision_score, recall_score, f1_score, multilabel_confusion_matrix, confusion_matrix
+from sklearn.metrics import classification_report
+
+# Ignore noise warning
+import warnings
+warnings.filterwarnings("ignore")
