c7016b03a9704e77493ca56c347447a80f91d0f2
Ganesh Jainarain <RicheyJay2@gmail.com>
2022-07-21 20:44:55 -0400

    getting familar with our data and its properties

--- Code/Main.py
+++ Code/Main.py
@@ -62,3 +62,29 @@ warnings.filterwarnings("ignore")
 #loading dataset
 data_set = pd.read_csv("/Users/richeyjay/Desktop/MBTI_ML/venv/Code/mbti_1.csv")
 print(data_set.tail())
+
+#Checking to see if there are any missing or null values in the dataset 
+print(data_set.isnull().any())
+
+#Getting an idea of the size of our dataset
+nRow, nCol = data_set.shape
+print(f'There are {nRow} rows and {nCol} columns')
+
+print(data_set.info())
+'''
+<class 'pandas.core.frame.DataFrame'>
+RangeIndex: 8675 entries, 0 to 8674
+Data columns (total 2 columns):
+ #   Column  Non-Null Count  Dtype 
+---  ------  --------------  ----- 
+ 0   type    8675 non-null   object
+ 1   posts   8675 non-null   object
+dtypes: object(2)
+memory usage: 135.7+ KB
+None
+'''
+#There are only 2 columns in the dataset
+#Total no. of rows are 8675
+#There are no null values present in the dataset
+#One Disadvantage is that all values are textual, 
+#hence they have to be converted to numerical form to train the ML model
